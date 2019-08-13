510992d28121722652d20c7c248b764265432095
Anton Nikanorov <anton.nikanorov@aplana.com>
2019-08-13 11:36:48 +0300

    update for dht22

--- dht.c
+++ dht.c
@@ -28,7 +28,6 @@ void read_dht11(char** data) {
 	digitalWrite(DHTPIN, HIGH);
 	delayMicroseconds(30);
 	pinMode(DHTPIN, INPUT);
- 
 	for ( i = 0; i < MAXTIMINGS; i++ ) {
 
 		counter = 0;
@@ -93,23 +92,19 @@ void read_dht22(char** data) {
 	dht_dat[0] = dht_dat[1] = dht_dat[2] = dht_dat[3] = dht_dat[4] = try_counter = 0;
  
 	pinMode(DHTPIN, OUTPUT);
-	
 	digitalWrite(DHTPIN, HIGH);
-	delay(10);
-	
+	delay(500);
+
 	digitalWrite(DHTPIN, LOW);
-	delay(18);
-	
-	digitalWrite(DHTPIN, HIGH);
-	delayMicroseconds(40);
+	delay(20);
 	pinMode(DHTPIN, INPUT);
- 
+	
 	for ( i = 0; i < MAXTIMINGS; i++ ) {
 
 		counter = 0;
 		while ( digitalRead(DHTPIN) == laststate ) {
 			counter++;
-			delayMicroseconds(1);
+			delayMicroseconds(2);
 			if ( counter == 255 ) {
 				break;
 			}
@@ -128,40 +123,41 @@ void read_dht22(char** data) {
 	    }
 	}
 
+//	printf("{%d, %d, %d, %d, %d}\n", dht_dat[0], dht_dat[1], dht_dat[2], dht_dat[3], dht_dat[4]);
+//	printf("%d ==  %d\n", dht_dat[4], ((dht_dat[0] + dht_dat[1] + dht_dat[2] + dht_dat[3]) & 0xFF));
+
+
 	if ( (j >= 40) && (dht_dat[4] == ( (dht_dat[0] + dht_dat[1] + dht_dat[2] + dht_dat[3]) & 0xFF) ) )	{
 
-       float t, h;
-       h = (float)dht_dat[0] * 256 + (float)dht_dat[1];
-       h /= 10;
-       t = (float)(dht_dat[2] & 0x7F)* 256 + (float)dht_dat[3];
-       t /= 10.0;
-       if ((dht_dat[2] & 0x80) != 0)  t *= -1;
+       		float t, h;
+	       	h = (float)dht_dat[0] * 256 + (float)dht_dat[1];
+       		h /= 10;
+	       	t = (float)(dht_dat[2] & 0x7F)* 256 + (float)dht_dat[3];
+       		t /= 10.0;
+	       	if ((dht_dat[2] & 0x80) != 0)  t *= -1;
 
-	   f = t * 9. / 5. + 32;
+       		f = t * 9. / 5. + 32;
 
-	   if(*data == NULL) {
-         *data = (char*)malloc(sizeof(char)*37);
-         memset(*data, 0, sizeof(char)*37);
-       }
+	       	if(*data == NULL) {
+        		 *data = (char*)malloc(sizeof(char)*37);
+		         memset(*data, 0, sizeof(char)*37);
+	       	}
            
-       char result[36];
-       sprintf(result, "H:%.1f%% T:%.1fC TF:%.1fF\n", h, t, f);
-       strcpy(*data, result);
+       		char result[36];
+	       	sprintf(result, "H:%.1f%% T:%.1fC TF:%.1fF\n", h, t, f);
+       		strcpy(*data, result);
 	   
-	} else {
-           
-	  ++try_counter;  
-	  if (MAXTIMINGS > try_counter) {
-	    read_dht22(data);
-	  } else {
-
-	    *data = (char*)malloc(sizeof(char)*37);
-	    memset(*data, 0, sizeof(char)*37);
-
-	    char result[36];
-	    sprintf(result, "problem with sensor");
-	    strcpy(*data, result);
-	  }
+	} else {          
+		  ++try_counter;  
+		  if (MAXTIMINGS > try_counter) {
+			read_dht22(data);
+		  } else {
+		    *data = (char*)malloc(sizeof(char)*37);
+		    memset(*data, 0, sizeof(char)*37);
+		    char result[36];
+	    	printf(result, "problem with sensor");
+		    strcpy(*data, result);
+		}
     }
 }
 
--- test.c
+++ test.c
@@ -7,7 +7,7 @@ int main(void) {
   char *result; 
 
   for(int i = 5; i > 0; i--) {
-     read_dht22(&result); 
+     read_dht22(&result);
      printf("%s \n", result);
   }
 
